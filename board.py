e6afd8801cc93498cdf1e3a708d14b1d7b060ff6
bambooom <nmz89825@gmail.com>
2015-12-13 17:17:38 +0800

    change sqlite3 to kvdb since sqlite not applicable in sae

--- board.py
+++ board.py
@@ -14,20 +14,21 @@ from flask import Flask, render_template, request, redirect, session, url_for, f
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import URL
-from dbhandler import fetch_repos_table, fetch_name_list, add_repo
+from dbhandler import fetch_repos, add_repo
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'OctoDog key'
+ 
+reponame_list = fetch_repos()
 
-#repos_table = 
-reponame_list = fetch_name_list(fetch_repos_table())
-
-def update_list():
-	global reponame_list
-	#repos_table = fetch_repos_table()
-	reponame_list = fetch_name_list(fetch_repos_table())
-	return reponame_list
+#def update_list():
+#	'''
+#	update the name list of repository
+#	'''
+#	global reponame_list
+#	reponame_list = fetch_name_list(fetch_repos_table())
+#	return reponame_list
 
 def get_repo_name(repo_url):
 	'''
@@ -38,31 +39,34 @@ def get_repo_name(repo_url):
 	return foo.path.split('/')[2]
 
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def board():
 	global reponame_list
-	reponame_list = update_list()
-	return render_template("index.html", repos = reponame_list)
+	reponame_list = fetch_repos()
+	return render_template("index.html", repos=reponame_list)
 
 
 class InsertPro(Form):
-	repo_url = StringField("Add your project", validators=[URL(message='\
-		Sorry, this is not a valid URL')])
+	repo_url = StringField("Add your project", validators=[URL(
+		message='Sorry, this is not a valid URL')])
 	submit = SubmitField('Submit')
 
 
 @app.route('/project', methods=['POST','GET'])
 def insert_pro():
+	'''
+	input url of repository and one click to add 
+	and redirect to new repository page
+	'''
 	form = InsertPro()
 	global reponame_list
 	if form.validate_on_submit():
 		session['repo_url'] = form.repo_url.data
 		url = session.get('repo_url')
 		reponame = get_repo_name(url)
-		new_repo = reponame, url
-		add_repo(new_repo)
-		reponame_list = update_list()
-		# flash("Added Successfully.")
+		new_repo = {'name':reponame, 'url':url}
+		add_repo(new_repo) # insert into database 
+		reponame_list = fetch_repos()
 		return redirect(url_for('show_pro', reponame=reponame, 
 			repos=reponame_list, _external=True))	
 	return render_template("project.html", repos=reponame_list, 
@@ -72,12 +76,17 @@ def insert_pro():
 def show_pro(reponame):
 	# show the project profile with info
 	global reponame_list
-	reponame_list = update_list()
+	reponame_list = fetch_repos()
 	return render_template("profile.html", reponame=reponame, 
 		repos=reponame_list)
 	#return 'showcase for project %s' % reponame
 
-
+@app.route('/about', methods=['GET'])
+def about_us():
+	# post README here
+	global reponame_list
+	reponame_list = fetch_repos()
+	return render_template("about.html", repos=reponame_list)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- dbhandler.py
+++ dbhandler.py
@@ -6,37 +6,51 @@ http://projboard.sinaapp.com/
 @author: bambooom
 '''
 
-import sqlite3
+import sae.kvdb
 import os
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-ROOT = os.path.dirname(os.path.abspath(__file__))
+#ROOT = os.path.dirname(os.path.abspath(__file__))
 
-def fetch_repos_table():
-    conn = sqlite3.connect(ROOT + '/Repository.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE if not exists repos (reponame text, repourl text)')
-    c.execute('CREATE UNIQUE INDEX if not exists id ON repos (reponame, repourl)')
-    c.execute('SELECT * FROM repos')
-    repos_table = [list(e) for e in c.fetchall()]
-    return repos_table
-    
-def fetch_name_list(repos_table):
-    name_list = [repos_table[i][0] for i in range(len(repos_table))]
+def fetch_repos():
+    kv = sae.kvdb.Client()
+    temp1 = [i[1] for i in list(kv.get_by_prefix("repo#"))]
+    temp2 = sorted(temp1, key = lambda x:x['name'])
+    name_list = [temp2[i]['name'] for i in range(len(temp2))]
+    kv.disconnect_all()
     return name_list
 
-def add_repo(new_repo):
-    conn = sqlite3.connect(ROOT + '/Repository.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE if not exists repos (reponame text, repourl text)')
-    c.execute('INSERT OR IGNORE INTO repos VALUES (?,?)', new_repo) 
-    # INSERT OR IGNORE to ignore the duplicate value input
-    conn.commit()
-    conn.close()
+    #conn = sqlite3.connect(ROOT + '/Repository.db')
+    #c = conn.cursor()
+    #c.execute('CREATE TABLE if not exists repos (reponame text, repourl text)')
+    #c.execute('CREATE UNIQUE INDEX if not exists id ON repos (reponame, repourl)')
+    #c.execute('SELECT * FROM repos')
+    #repos_table = [list(e) for e in c.fetchall()]
+    #return repos_table
+    
+#def fetch_name_list(repos_table):
+#   name_list = [repos_table[i][0] for i in range(len(repos_table))]
+#    return name_list
 
-#new_repo=('OctoDog','https://github.com/OctoPuppy')
+def add_repo(new_repo):
+    kv = sae.kvdb.Client()
+    key = "repo#" + str(len(fetch_repos()))
+    kv.set(key,new_repo)
+    kv.disconnect_all()
+
+
+
+    #conn = sqlite3.connect(ROOT + '/Repository.db')
+    #c = conn.cursor()
+    #c.execute('CREATE TABLE if not exists repos (reponame text, repourl text)')
+    #c.execute('INSERT OR IGNORE INTO repos VALUES (?,?)', new_repo) 
+    # INSERT OR IGNORE to ignore the duplicate value input
+    #conn.commit()
+    #conn.close()
+
+#new_repo=('OctoDog','https://github.com/OctoPuppy/Octodog')
 #add_repo(new_repo)
 #add_repo(new_repo)
 #add_repo(new_repo)
